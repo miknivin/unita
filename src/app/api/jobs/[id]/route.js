c77b321a49604a80795aa16724f9bc765aedb3de
miknivin <116654570+miknivin@users.noreply.github.com>
2025-04-05 00:23:42 +0530

    dbconnect added

--- src/app/api/jobs/[id]/route.js
+++ src/app/api/jobs/[id]/route.js
@@ -2,11 +2,13 @@
 
 import { NextResponse } from "next/server";
 import { JobPost } from "@/models/JobpostSchema.js";
+import dbConnect from "@/lib/db/connection";
 
 // GET: Retrieve a job post by ID
 export async function GET(req, { params }) {
   try {
     const { id } = await params;
+    await dbConnect();
     const jobPost = await JobPost.findById(id).populate("company");
     if (!jobPost) {
       return NextResponse.json(
